e8c799f0047988a627332a603fc056daa9f987d7
ryanwonghc <rhcw@umich.edu>
2023-10-27 22:52:26 -0400

    dynamodb

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,3 +28,33 @@ resource "aws_budgets_budget" "scheduler-budget" {
     subscriber_email_addresses = ["ryanwonghc55@gmail.com"]
   }
 }
+
+# dynamoDB table
+resource "aws_dynamodb_table" "scheduler-db" {
+  name            = "scheduler-db"
+  billing_mode    = "PROVISIONED"
+  read_capacity   = "20"
+  write_capacity  = "20"
+  hash_key        = "UserId"
+  range_key       = "TaskId"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  attribute {
+    name = "TaskId"
+    type = "S"
+  }
+
+  ttl {
+    enabled = true
+    attribute_name = "expiryPeriod"
+  }
+
+  point_in_time_recovery {
+    enabled = true
+  }
+
+}
